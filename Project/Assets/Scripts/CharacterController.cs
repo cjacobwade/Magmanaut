a499ba9b033b306a30e401006674130019f51a7b
Christopher Wade <cjacobwade@gmail.com>
2013-06-13 02:54:08 -0500

    Adding assets and gitignore

---
+++ Project/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+	
+	//Movement
+		//Move speed
+		
+	
+	//Jumping
+		//Max height or jump speed
+	
+	
+	//Gravity
+	
+	
+	//Animation
+	
+	
+	//Sound
+	
+	
+	//Health
+	
+	
+	//Swipe
+	
+	
+	
+	
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
