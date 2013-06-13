3276360752840f112db82c4a7a3555317d7ee8e3
Braden Polly <sting@neo.rr.com>
2013-06-13 18:24:02 -0400

    Fixed Start Screen

--- Project/Assets/Scripts/TitleScreenTest.cs
+++ Project/Assets/Scripts/TitleScreenTest.cs
@@ -4,9 +4,18 @@ using System.Collections;
 public class TitleScreenTest : MonoBehaviour {
 	
 	
-public Texture2D buttonImage; 	
+	public Texture2D buttonImage;
+	public Texture2D MagmanautTitle;
 	int xpos;
 	int ypos;
+	public int changeSizeX_button;
+	public int changeSizeY_button;
+	public int changeSizeX_title;
+	public int changeSizeY_title;
+	
+	void Start(){
+		Screen.orientation = ScreenOrientation.Landscape;
+	}
 	
 	void Update(){
 	xpos = (Screen.width/2)-(buttonImage.width/2);
@@ -14,8 +23,9 @@ public Texture2D buttonImage;
 	}
 	
 	void OnGUI() {
-        GUI.Button(new Rect(xpos,ypos,buttonImage.width,buttonImage.height), buttonImage);
-        
+		GUI.Label (new Rect (xpos-changeSizeX_title,ypos-changeSizeY_title, MagmanautTitle.width, MagmanautTitle.height),MagmanautTitle);
+        if (GUI.Button(new Rect(xpos,ypos,buttonImage.width-changeSizeX_button,buttonImage.height-changeSizeY_button), buttonImage)){
+        Application.LoadLevel("Testing");}
 
     }
 }
